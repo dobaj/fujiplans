f14c614d4746e0edb0ca9dd1a58f8ba8a904a36a
Aqothy <aq0335@gmail.com>
2024-09-28 19:44:03 -0400

    new user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class User(models.Model):
-    email = models.EmailField
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
+
