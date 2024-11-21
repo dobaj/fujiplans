76df535769249390ecd234ab18e85b04f91bdc10
Aqothy <aq0335@gmail.com>
2024-11-20 20:55:53 -0500

    super cool card hover animation to display samples of our pdf

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,19 @@
-
 import Link from "next/link";
+import Pdfanimation from "@/components/common/Pdfanimation";
 
 export default function Page() {
-
   return (
-      <main>
-        <p>hello world</p>
-        <Link href={"/login"} className="mr-5">
-          Login
-        </Link>
-        <Link href={"/register"}>Register</Link>
-      </main>
+    <main className="h-full w-full bg-black">
+      <p className="text-white">hello world</p>
+      <Link href={"/login"} className="mr-5 text-white">
+        Login
+      </Link>
+      <Link href={"/register"} className="text-white">
+        Register
+      </Link>
+      <div className="h-[80%] w-full flex justify-center items-center">
+        <Pdfanimation />
+      </div>
+    </main>
   );
 }
